8dcf80a03a67b6e752a3202eef1aebbe66af7929
Junseo Kim <semiwest2@gmail.com>
2024-11-28 17:19:47 +0900

    feat(server): Express 서버 설정 및 기본 라우트 연결

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import healthRoute from './routes/healthRoute.js';
+import healthRouter from './routes/healthRoute.js';
+import authRouter  from './routes/authRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors())
 
-app.use('/health', healthRoute);
+app.use('/health', healthRouter);
+app.use('/auth', authRouter );
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
